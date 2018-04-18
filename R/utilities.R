6ed616a9344e118171d8835d1fc0b97f2ab6df74
Grégoire Lurton <gregoire.lurton@gmail.com>
2018-04-18 22:27:13 +0200

    Creating list of Months in a period
    FUnction in utilities that creates the full list of months between two dates. Used for querying analytics

--- R/utilities.R
+++ R/utilities.R
@@ -26,3 +26,32 @@ df_from_org_unit_description <- function(org_units_description_list){
   )
   return(out)
 }
+
+
+period_to_months <- function(period_start, period_end){
+  year_start <- substr(period_start, 1, 4)
+  year_end <- substr(period_end, 1, 4)
+  years <- seq(year_start, year_end, 1)
+  months <- seq(1,12)
+  month_year_1 <- seq(substr(period_start, 5, 6) , 12)
+  month_year_n <- seq(1, substr(period_end, 5, 6))
+  if (length(years) == 1){
+    months <- seq(substr(period_start, 5, 6) , substr(period_end, 5, 6))
+    out <- paste0(year_start, months , concatenate = "")
+  }
+  if (length(years) == 2){
+    out_1 <- paste0(year_start, month_year_1)
+    out_2 <- paste0(year_end, month_year_n)
+    out <- c(out_1, out_2)
+  }
+  if (length(years) > 2){
+    years <- years[-c(1,length(years))]
+    out_1 <- paste0(year_start, month_year_1)
+    out_2 <- apply(expand.grid(years, months), 1, paste, collapse="")
+    out_n <- paste0(year_end, month_year_n)
+    out <- c(out_1, out_2, out_n)
+
+  }
+  out[nchar(out) == 5] <- paste0(substr(out[nchar(out) == 5],1,4) , '0' , substr(out[nchar(out) == 5],5,5))
+  return(sort(out))
+}
